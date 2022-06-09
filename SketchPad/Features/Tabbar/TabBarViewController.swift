f0e0439588292fc2e9c491c9fcbe34c5b3c15c17
Asmaa <asmaatarek93@gmail.com>
2022-06-10 00:56:56 +0200

    Add TabBarViewController

---
+++ SketchPad/Features/Tabbar/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  SketchPad
+//
+//  Created by Asmaa Tarek on 10/06/2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
