b9d42d806ade3e7b590b34bce6e95c99dd67093f
Asmaa <asmaatarek93@gmail.com>
2022-06-10 01:57:18 +0200

    Setup tabbar controllers

--- SketchPad/Features/Tabbar/TabBarCoordinator.swift
+++ SketchPad/Features/Tabbar/TabBarCoordinator.swift
@@ -24,9 +24,40 @@ class TabBarCoordinator {
     }
 }
 
+fileprivate extension TabBarCoordinator {
+    func getTabBarTab(_ tab: Tab) -> UINavigationController {
+        let rootViewController = viewController(for: tab)
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.isNavigationBarHidden = true
+        navigationController.tabBarItem.title = tab.tabTitle
+        navigationController.tabBarItem.image = UIImage.getImage(from: tab.imageName).withRenderingMode(.alwaysTemplate)
+        return navigationController
+     }
+     
+    func viewController(for tab: Tab) -> UIViewController {
+         var viewController = UIViewController()
+        switch tab {
+        case .drawing:
+            viewController = DrawingViewController()
+        case .history:
+            viewController = HistoryViewController()
+        }
+        return viewController
+     }
+    
+    func prepareTabBarController(with tabControllers: [UIViewController]) {
+        tabBarController.setViewControllers(tabControllers, animated: true)
+        tabBarController.tabBar.backgroundColor = UIColor.color(for: .tabbarBackgroundColor)
+        UITabBar.appearance().tintColor = .color(for: .tintColor)
+    }
+}
+
 extension TabBarCoordinator: TabBarCoordinatorProtocol {
      
     func start() {
+        let tabs = Tab.allCases
+        let controllers = tabs.map(getTabBarTab(_:))
+        prepareTabBarController(with: controllers)
     }
     
 }
