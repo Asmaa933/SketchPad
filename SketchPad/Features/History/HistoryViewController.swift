1a08c6d96618cf059ab9bf56e0377cd2bb59efc8
Asmaa <asmaatarek93@gmail.com>
2022-06-10 00:59:08 +0200

    Add HistoryViewController

---
+++ SketchPad/Features/History/HistoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryViewController.swift
+//  SketchPad
+//
+//  Created by Asmaa Tarek on 10/06/2022.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
