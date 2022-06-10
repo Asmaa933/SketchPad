50ee342c310e515be9422085563bf95ade146bfa
Asmaa <asmaatarek93@gmail.com>
2022-06-10 14:01:45 +0200

    Initialize and connect outlets, actions for DrawingActionsView

--- SketchPad/Features/Drawing/Views/DrawingActionsView/DrawingActionsView.swift
+++ SketchPad/Features/Drawing/Views/DrawingActionsView/DrawingActionsView.swift
@@ -7,7 +7,44 @@
 
 import UIKit
 
+fileprivate enum SketchActionButton {
+    case close
+    case undo
+    case redo
+    case delete
+    case done
+}
+
 class DrawingActionsView: UIView {
     
-   
+    @IBOutlet private weak var closeButton: UIButton!
+    @IBOutlet private weak var undoButton: UIButton!
+    @IBOutlet private weak var redoButton: UIButton!
+    @IBOutlet private weak var deleteButton: UIButton!
+    @IBOutlet private weak var doneButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNibView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNibView()
+    }
+    
+    @IBAction private func closeButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction private func undoButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction private func redoButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction private func deleteButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction private func doneButtonAction(_ sender: UIButton) {
+    }
 }
