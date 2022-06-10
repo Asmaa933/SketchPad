a2c646547d10df1194aa3800c172053149d2757f
Asmaa <asmaatarek93@gmail.com>
2022-06-10 13:42:39 +0200

    Create SketchView

---
+++ SketchPad/Features/Drawing/Views/SketchView/SketchView.swift
@@ -0,0 +1,20 @@
+//
+//  DrawingView.swift
+//  SketchPad
+//
+//  Created by Asmaa Tarek on 10/06/2022.
+//
+
+import UIKit
+
+class SketchView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
