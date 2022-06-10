3764f15c92fca966d26c39804fc61fd96ea7d46b
Asmaa <asmaatarek93@gmail.com>
2022-06-10 17:29:41 +0200

    Check for image permission before get it

--- SketchPad/Features/Drawing/ViewModel/DrawingViewModel.swift
+++ SketchPad/Features/Drawing/ViewModel/DrawingViewModel.swift
@@ -25,13 +25,29 @@ class DrawingViewModel {
     
 }
 
-extension DrawingViewModel: DrawingViewModelProtocol {
+fileprivate extension DrawingViewModel {
     
-    func getImage() {
+    func getImageFromCoordinator() {
         coordinator.imageDidPicked = {[weak self] imageData in
             guard let self = self else { return }
             self.imageDidPicked?(imageData)
         }
         coordinator.showImagePicker()
+    }
+}
+
+extension DrawingViewModel: DrawingViewModelProtocol {
+    
+    func getImage() {
+        let photoLibraryManager: PhotoLibraryManagerProtocol = PhotoLibraryManager()
+        photoLibraryManager.getPermission {[weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(_):
+                self.getImageFromCoordinator()
+            case .failure(let error):
+                debugPrint("permission denied")
+            }
+        }
     }
 }
---
+++ SketchPad/Helper/Constants/AppError.swift
@@ -0,0 +1,12 @@
+//
+//  AppError.swift
+//  SketchPad
+//
+//  Created by Asmaa Tarek on 10/06/2022.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case photoPermissionDenied
+}
